c89568ae3bbb01d9af00947cd89f9f5f7b1c6587
Wes <wes.delange@gmail.com>
2018-04-24 08:55:47 -0400

    Set up basic framework for landing page

---
+++ index.js
@@ -0,0 +1,28 @@
+$(function (){
+    $('.sections').on('change', function (){
+        var value = $('select[name=sections]').val()
+        
+        var url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
+        url += '?' + $.param({'api-key': "2ecd6c760c464c138f0d96da0aaa5164"});
+            
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function(data){
+            $.each(data, function(key, value){
+                console.log(data);
+                ('.news').append('<p>' + data.results[0].abstract + '</p>')
+            });
+            $.fail(function(err) {
+                throw err;
+              });
+        });
+    });
+});
+
+
+
+
+
+
+
