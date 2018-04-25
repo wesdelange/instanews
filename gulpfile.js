310a26d990718f29ff036a6ce654fb9424c2144a
Wes <wes.delange@gmail.com>
2018-04-25 16:51:51 -0400

    Added Sass, worked on JavaScript,CSS and HTML

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,43 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
 var eslint = require('gulp-eslint');
 
-gulp.task('lint', function () {
-    return gulp
-    .src(['/js/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError);
+var sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  cssnano = require("gulp-cssnano"),
+  rename = require("gulp-rename");
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/style.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./build/css"))
+    .pipe(cssnano())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("./build/css"));
 });
+
+
+
 gulp.task('scripts', function(){
-  return gulp.src('./js/*.js')
+  return gulp
+    .src('./js/*.js')
     .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
+    .pipe(
+        rename({ 
+            extname: '.min.js' }))
     .pipe(gulp.dest('./build/js'))
 });
 
 gulp.task('watch', function() {
     gulp.watch('js/*.js', gulp.series('scripts'));
+    gulp.watch('sass/*.scss', gulp.series('sass'));
  });
  gulp.task('browser-sync', function() {
     browserSync.init({
@@ -28,5 +46,17 @@ gulp.task('watch', function() {
             baseDir: "./"
         }
     });
+    gulp
+    .watch(['build/css/*.css', 'build/js/*.js'])
+    .on('change', browserSync.reload);
 });
+
+gulp.task('lint', function() {
+    return gulp
+      .src(['/js/*.js'])
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError);
+  });
+
 gulp.task('default', gulp.parallel('browser-sync', 'watch'));
---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(function (){
+    $('.sections').on('change', function (){
+        $('header').css("height", "auto")
+        var value = $('select[name=sections]').val()
+        
+        var url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
+        url += '?' + $.param({'api-key': "2ecd6c760c464c138f0d96da0aaa5164"});
+            
+        $.ajax({
+            url: url,
+            method: 'GET',
+        })
+        .done(function(data) { 
+            $('.news').empty();
+            $.each(data.results, function(key, value){ 
+                var abstract = value.abstract;
+                var link = value.url;
+                var img = value.multimedia[1].url;
+                $('.news').append('<article>' + '<a href=' + link + '>' + '<img src=' + img + '>' + '</a>' + '<div>' + '<p>' + abstract + '</p>' + '</div>' + '</article>')
+                return key < 11            
+            });
+        }).fail(function(err) {
+            console.log(err);
+        })
+    });
+});
+
+
+
+
+
+
+
