cc626c13873abcd296f6cbe1526b52c298d7d305
Wes <wes.delange@gmail.com>
2018-04-26 15:08:33 -0400

    fixed hover bug

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-$(function (){
+$(function() {
+    $('select').selectric();
+});
+$(function (){ 
     $('.sections').on('change', function (){
         $('header').css("height", "auto")
-        var value = $('select[name=sections]').val()
-        
+        var value = $('select[name=sections]').val()        
         var url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
         url += '?' + $.param({'api-key': "2ecd6c760c464c138f0d96da0aaa5164"});
-            
         $.ajax({
             url: url,
             method: 'GET',
