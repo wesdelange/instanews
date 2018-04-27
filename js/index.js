29ffd3a74e863c566cdfe5ca295d4511f2dda14e
Wes <wes.delange@gmail.com>
2018-04-27 15:18:28 -0400

    added if statement to skip articles without image

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
 $(function (){ 
     $('.sections').on('change', function (){
         $('header').css("height", "auto")
+        $('.news').empty();
+        $('.news').append('<div class="loading"><img id="gif" src="../project-02/assets/images/ajax-loader.gif" alt="loading..."><div>');
         var value = $('select[name=sections]').val()        
         var url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
         url += '?' + $.param({'api-key': "2ecd6c760c464c138f0d96da0aaa5164"});
@@ -12,21 +14,17 @@ $(function (){
         .done(function(data) { 
             $('.news').empty();
             $.each(data.results, function(key, value){ 
+                console.log(value);
                 var abstract = value.abstract;
                 var link = value.url;
-                var img = value.multimedia[4].url;
-                $('.news').append('<article>' + '<a href=' + link + '>' + '<img src=' + img + '>' + '</a>' + '<div>' + '<p>' + abstract + '</p>' + '</div>' + '</article>')
-                return key < 11            
+                var img = value.multimedia.length > 0 ? value.multimedia[4].url : false;
+                if (img) {
+                $('.news').append('<article>' + '<a href=' + link + '>' + '<img src=' + img + '>' + '</a>' + '<div>' + '<p>' + abstract + '</p>' + '</div>' + '</article>');
+                } 
+                return key < 11;            
             });
         }).fail(function(err) {
             console.log(err);
         })
     });
 });
-
-
-
-
-
-
-
