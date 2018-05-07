3febccb403823a3e26488d30af23a418e4ebbae3
Wes <wes.delange@gmail.com>
2018-05-07 14:44:10 -0400

    added babel, changed index.js to es6

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
 
-$(function (){ 
-    $('.sections').on('change', function (){
+$(() => { 
+    $('.sections').on('change',() => {
         $('header').css("height", "auto")
         $('.news').empty();
         $('.news').append('<div class="loading"><img id="gif" src="/project-02/assets/images/ajax-loader.gif" alt="loading..."><div>');
-        var value = $('select[name=sections]').val()        
-        var url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
+        let value = $('select[name=sections]').val()        
+        let url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
         url += '?' + $.param({'api-key': "2ecd6c760c464c138f0d96da0aaa5164"});
         $.ajax({
             url: url,
             method: 'GET',
         })
-        .done(function(data) { 
+        .done((data) => { 
             $('.news').empty();
-            $.each(data.results, function(key, value){ 
+            $.each(data.results,(key, value) => { 
                 console.log(value);
-                var abstract = value.abstract;
-                var link = value.url;
-                var img = value.multimedia.length > 0 ? value.multimedia[4].url : false;
+                let abstract = value.abstract;
+                let link = value.url;
+                let img = value.multimedia.length > 0 ? value.multimedia[4].url : false;
                 if (img) {
                 $('.news').append('<article>' + '<a href=' + link + '>' + '<img src=' + img + '>' + '</a>' + '<div>' + '<p>' + abstract + '</p>' + '</div>' + '</article>');
                 } 
                 return key < 11;            
             });
-        }).fail(function(err) {
+        }).fail((err) => {
             console.log(err);
         })
     });
